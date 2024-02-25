4b4382243cab2da0420443058bf7242ecb7b84ae
Nguyễn Đức An <an.nguyenduc1406@hcmut.edu.vn>
2024-02-25 15:16:21 +0700

    feat: update invitation email to classroom api

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -205,6 +205,7 @@ export class AuthController {
     'https://www.googleapis.com/auth/classroom.coursework.students',
     'https://www.googleapis.com/auth/classroom.student-submissions.me.readonly',
     'https://www.googleapis.com/auth/classroom.student-submissions.students.readonly',
+    'https://www.googleapis.com/auth/classroom.rosters',
   ])
   @ApiQuery({
     name: 'fromUser',
