18a5583357d7efd9365a133083e766404bfb6910
Quang Khánh <khanhnguyenmcu@gmail.com>
2024-03-06 12:46:19 +0700

    Update robot entity; upload robot code to S3

--- src/robot/entity/robot.entity.ts
+++ src/robot/entity/robot.entity.ts
@@ -1,43 +1,27 @@
-import { Entity, Column, ManyToOne, PrimaryGeneratedColumn, CreateDateColumn } from 'typeorm';
+import { Entity, Column, ManyToOne, PrimaryColumn, CreateDateColumn } from 'typeorm';
 import { User } from 'src/users/entity/user.entity';
 import { Process } from 'src/processes/entity/process.entity';
 
 @Entity()
 export class Robot {
-  @PrimaryGeneratedColumn('uuid')
-  id: string;
-
   @Column({
     nullable: false,
   })
   name: string;
 
-  // RobotFramework code (JSON format, stringified)  
-  @Column({
-    nullable: false,
-    type: 'longtext'
-  })
-  code: string;
-
-  @Column({
-    nullable: false,
-  })
+  @PrimaryColumn()
   userId: number;
 
   @ManyToOne(() => User, (user) => user.id)
   user: User;
 
-  @Column({
-    nullable: false,
-  })
+  @PrimaryColumn()
   processId: string;
 
   @ManyToOne(() => Process, (process) => process.id)
   process: Process;
 
-  @Column({
-    nullable: false,
-  })
+  @PrimaryColumn()
   processVersion: number;
 
   @CreateDateColumn()
