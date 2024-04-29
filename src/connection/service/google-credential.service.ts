f1c9c6dbdf23f360607399d9e680a36c4d2867ab
David Huynh <78911364+02david20@users.noreply.github.com>
2024-04-29 22:15:09 +0700

    Update create robot credential file with scopes

--- src/connection/service/google-credential.service.ts
+++ src/connection/service/google-credential.service.ts
@@ -30,9 +30,13 @@ export class GoogleCredentialService implements ICredentialService {
             client_secret: process.env["GOOGLE_DRIVE_CLIENT_SECRET"],
             token_uri: "https://oauth2.googleapis.com/token",
             scopes: [
-                "https://www.googleapis.com/auth/drive",
-                "email",
-                "profile"
+                'https://www.googleapis.com/auth/drive',
+                'https://www.googleapis.com/auth/drive.appdata',
+                'https://www.googleapis.com/auth/drive.metadata',
+                'https://www.googleapis.com/auth/drive.install',
+                'https://www.googleapis.com/auth/drive.file',
+                'email', 
+                'profile'
             ]
         }
     }
@@ -45,7 +49,13 @@ export class GoogleCredentialService implements ICredentialService {
             client_secret: process.env["GMAIL_CLIENT_SECRET"],
             token_uri: "https://oauth2.googleapis.com/token",
             scopes: [
-                "https://mail.google.com/"
+                "https://www.googleapis.com/auth/gmail.send",
+                "https://www.googleapis.com/auth/gmail.compose",
+                "https://www.googleapis.com/auth/gmail.modify",
+                "https://www.googleapis.com/auth/gmail.labels", 
+                'https://mail.google.com/', 
+                'email', 
+                'profile'
             ]
         }
     }
@@ -57,7 +67,20 @@ export class GoogleCredentialService implements ICredentialService {
             client_id: process.env["GOOGLE_CLASSROOM_CLIENT_ID"],
             client_secret: process.env["GOOGLE_CLASSROOM_CLIENT_SECRET"],
             token_uri: "https://oauth2.googleapis.com/token",
-            scopes: []
+            scopes: [
+                'https://www.googleapis.com/auth/classroom.courses',
+                'https://www.googleapis.com/auth/classroom.announcements',
+                'https://www.googleapis.com/auth/classroom.topics',
+                'https://www.googleapis.com/auth/classroom.courseworkmaterials',
+                'https://www.googleapis.com/auth/classroom.coursework.me',
+                'https://www.googleapis.com/auth/classroom.coursework.students',
+                'https://www.googleapis.com/auth/classroom.student-submissions.me.readonly',
+                'https://www.googleapis.com/auth/classroom.student-submissions.students.readonly',
+                'https://www.googleapis.com/auth/classroom.rosters',
+                'https://www.googleapis.com/auth/classroom.profile.emails',
+                'email',
+                'profile',
+            ]
         }
     }
     
@@ -68,7 +91,13 @@ export class GoogleCredentialService implements ICredentialService {
             client_id: process.env["GOOGLE_FORMS_CLIENT_ID"],
             client_secret: process.env["GOOGLE_FORMS_CLIENT_SECRET"],
             token_uri: "https://oauth2.googleapis.com/token",
-            scopes: []
+            scopes: [
+                'https://www.googleapis.com/auth/forms.body', 
+                'https://www.googleapis.com/auth/forms.responses.readonly',
+                'https://www.googleapis.com/auth/drive',
+                'email', 
+                'profile',
+            ]
         }
     }
 
@@ -79,7 +108,7 @@ export class GoogleCredentialService implements ICredentialService {
             client_id: process.env["GOOGLE_SHEETS_CLIENT_ID"],
             client_secret: process.env["GOOGLE_SHEETS_CLIENT_SECRET"],
             token_uri: "https://oauth2.googleapis.com/token",
-            scopes: []
+            scopes: ['https://www.googleapis.com/auth/spreadsheets', 'email', 'profile']
         }
     }
 
