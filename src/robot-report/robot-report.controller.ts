b382ebdcbc7a7a6828d02224fbeede7ee11eb3ba
Nguyễn Đức An <an.nguyenduc1406@hcmut.edu.vn>
2024-04-29 16:31:42 +0700

    feat: update robot report apis

--- src/robot-report/robot-report.controller.ts
+++ src/robot-report/robot-report.controller.ts
@@ -1,8 +1,15 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { RobotReportService } from './robot-report.service';
 import { UserDecor } from 'src/common/decorators/user.decorator';
 import { UserPayload } from 'src/auth/strategy/jwt.strategy';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiQuery,
+  ApiResponse,
+  ApiTags,
+  getSchemaPath,
+} from '@nestjs/swagger';
 
 @Controller('robot-report')
 @ApiTags('robot-report')
@@ -10,18 +17,82 @@ import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 export class RobotReportController {
   constructor(private readonly robotRunDetailService: RobotReportService) {}
 
-  @Get('/run-detail/:streamID/:processID/:version')
+  @Get('/detail')
   async fetchRobotRunDetails(
     @UserDecor() user: UserPayload,
-    @Param('streamID') streamID: string,
-    @Param('processID') processID: string,
-    @Param('version') version: number,
+    @Query('streamID') streamID: string,
+    @Query('processID') processID: string,
+    @Query('version') version: number,
   ) {
-    return this.robotRunDetailService.getRobotRunDetails(
+    return this.robotRunDetailService.getRobotRunDetailCommands(
       streamID,
       user.id,
       processID,
       version,
     );
+  }
+
+  @Get('/overall')
+  async fetchRobotRunOverall(
+    @UserDecor() user: UserPayload,
+    @Query('processID') processID: string,
+    @Query('version') version: number,
+    @Query('date') date?: string,
+    @Query('passed') passed?: number,
+  ) {
+    return this.robotRunDetailService.getRobotRunTimeOverall(
+      processID,
+      user.id,
+      version,
+      passed,
+      date,
+    );
+  }
+
+  @Get('/overall/average')
+  async fetchRobotRunDetailAverage(
+    @UserDecor() user: UserPayload,
+    @Query('processID') processID: string,
+    @Query('version') version: number,
+    @Query('date') date?: string,
+    @Query('passed') passed?: number,
+  ) {
+    return this.robotRunDetailService.getAverageExecutionTime(
+      processID,
+      user.id,
+      version,
+      passed,
+      date,
+    );
+  }
+
+  @Get('/overall/group-passed')
+  async fetchRobotRunDetailStatisticPassed(
+    @UserDecor() user: UserPayload,
+    @Query('processID') processID: string,
+    @Query('version') version: number,
+    @Query('date') date?: string,
+  ) {
+    return this.robotRunDetailService.getCountsGroupedByPassed(processID, user.id, version, date);
+  }
+
+  @Get('/overall/failures')
+  async fetchRobotRunDetailFailures(
+    @UserDecor() user: UserPayload,
+    @Query('processID') processID: string,
+    @Query('version') version: number,
+    @Query('date') date?: string,
+  ) {
+    return this.robotRunDetailService.getFailedExecution(processID, user.id, version, date);
+  }
+
+  @Get('/overall/group-error')
+  async fetchRobotRunDetailStatisticError(
+    @UserDecor() user: UserPayload,
+    @Query('processID') processID: string,
+    @Query('version') version: number,
+    @Query('date') date?: string,
+  ) {
+    return this.robotRunDetailService.getCountsGroupedByError(processID, user.id, version, date);
   }
 }
--- src/robot-report/robot-report.service.ts
+++ src/robot-report/robot-report.service.ts
@@ -16,7 +16,12 @@ export class RobotReportService {
     private readonly robotRunOverallRepository: Repository<RobotRunOverall>,
   ) {}
 
-  async getRobotRunDetails(uuid: string, userId: number, processId: string, version: number) {
+  async getRobotRunDetailCommands(
+    uuid: string,
+    userId: number,
+    processId: string,
+    version: number,
+  ) {
     const queryBuilder = this.robotRunDetailRepository.createQueryBuilder('robot_run_detail');
 
     queryBuilder
@@ -38,6 +43,138 @@ export class RobotReportService {
       .andWhere('robot_run_detail.process_id = :processId', { processId })
       .andWhere('robot_run_detail.version = :version', { version })
       .orderBy('robot_run_detail.kw_id', 'ASC');
+
+    const results = await queryBuilder.getRawMany();
+
+    return results;
+  }
+
+  async getRobotRunTimeOverall(
+    processId: string,
+    userId: number,
+    version: number,
+    passed?: number,
+    date?: string,
+  ) {
+    const queryBuilder = this.robotRunOverallRepository.createQueryBuilder('robot_run_overall');
+
+    queryBuilder
+      .select([
+        'robot_run_overall.uuid',
+        'robot_run_overall.start_time',
+        'robot_run_overall.end_time',
+        'TIMESTAMPDIFF(SECOND, robot_run_overall.start_time, robot_run_overall.end_time) AS time_execution',
+      ])
+      .where('robot_run_overall.process_id = :processId', { processId })
+      .andWhere('robot_run_overall.user_id = :userId', { userId })
+      .andWhere('robot_run_overall.version = :version', { version })
+      .andWhere('robot_run_overall.passed = :passed', { passed });
+
+    if (date) {
+      queryBuilder.andWhere('DATE(robot_run_overall.start_time) = :date', { date });
+    }
+
+    const results = await queryBuilder.getRawMany();
+
+    return results;
+  }
+
+  async getAverageExecutionTime(
+    processId: string,
+    userId: number,
+    version: number,
+    passed?: number,
+    date?: string,
+  ) {
+    const queryBuilder = this.robotRunOverallRepository.createQueryBuilder('robot_run_overall');
+
+    queryBuilder
+      .select(
+        'AVG(TIMESTAMPDIFF(SECOND, robot_run_overall.start_time, robot_run_overall.end_time))',
+        'avg_time_execution',
+      )
+      .where('robot_run_overall.process_id = :processId', { processId })
+      .andWhere('robot_run_overall.user_id = :userId', { userId })
+      .andWhere('robot_run_overall.version = :version', { version })
+      .andWhere('robot_run_overall.passed = :passed', { passed });
+
+    if (date) {
+      queryBuilder.andWhere('DATE(robot_run_overall.start_time) = :date', { date });
+    }
+
+    const result = await queryBuilder.getRawOne();
+
+    return result;
+  }
+
+  async getCountsGroupedByPassed(
+    processId: string,
+    userId: number,
+    version: number,
+    date?: string,
+  ) {
+    const queryBuilder = this.robotRunOverallRepository.createQueryBuilder('robot_run_overall');
+
+    queryBuilder
+      .select('robot_run_overall.passed', 'passed')
+      .addSelect('COUNT(*)', 'count')
+      .where('robot_run_overall.process_id = :processId', { processId })
+      .andWhere('robot_run_overall.user_id = :userId', { userId })
+      .andWhere('robot_run_overall.version = :version', { version });
+
+    if (date) {
+      queryBuilder.andWhere('DATE(robot_run_overall.start_time) = :date', { date });
+    }
+
+    queryBuilder.groupBy('robot_run_overall.passed');
+
+    const results = await queryBuilder.getRawMany();
+
+    return results;
+  }
+
+  async getFailedExecution(processId: string, userId: number, version: number, date?: string) {
+    const queryBuilder = this.robotRunOverallRepository.createQueryBuilder('robot_run_overall');
+
+    queryBuilder
+      .select('robot_run_overall.uuid', 'uuid')
+      .addSelect('robot_run_overall.passed', 'passed')
+      .addSelect('robot_run_overall.error_message', 'error_message')
+      .addSelect('robot_run_overall.start_time', 'start_time')
+      .addSelect('robot_run_overall.end_time', 'end_time')
+      .addSelect(
+        'TIMESTAMPDIFF(SECOND, robot_run_overall.start_time, robot_run_overall.end_time)',
+        'time_execution',
+      )
+      .where('robot_run_overall.process_id = :processId', { processId })
+      .andWhere('robot_run_overall.user_id = :userId', { userId })
+      .andWhere('robot_run_overall.version = :version', { version })
+      .andWhere('robot_run_overall.passed = :passed', { passed: 0 });
+
+    if (date) {
+      queryBuilder.andWhere('DATE(robot_run_overall.start_time) = :date', { date });
+    }
+
+    const results = await queryBuilder.getRawMany();
+
+    return results;
+  }
+
+  async getCountsGroupedByError(processId: string, userId: number, version: number, date?: string) {
+    const queryBuilder = this.robotRunOverallRepository.createQueryBuilder('robot_run_overall');
+
+    queryBuilder
+      .select('robot_run_overall.error_message', 'error_message')
+      .addSelect('COUNT(*)', 'count')
+      .where('robot_run_overall.process_id = :processId', { processId })
+      .andWhere('robot_run_overall.user_id = :userId', { userId })
+      .andWhere('robot_run_overall.version = :version', { version });
+
+    if (date) {
+      queryBuilder.andWhere('DATE(robot_run_overall.start_time) = :date', { date });
+    }
+
+    queryBuilder.groupBy('robot_run_overall.error_message');
 
     const results = await queryBuilder.getRawMany();
 
